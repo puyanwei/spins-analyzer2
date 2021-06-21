2fd9808d132f590965836170352e71f8258df766
Puyan Wei <puyanwei@gmail.com>
2021-06-21 18:55:06 +0100

    Added percentages to prizepool pie chart

--- frontend/src/Utilities/transformForPrizepool.js
+++ frontend/src/Utilities/transformForPrizepool.js
@@ -1,19 +1,26 @@
+// Initial data is a array of objects of all data, prizepool data is extracted and then put into another hash where the key is the prizepool and the value is the count of prizepools that it has seen. Then this data needs to be transformed into {x: key, y: value} format for victory.js Pie chart's data prop
+
 const transformForPrizepool = (data) => {
   const hash = {};
   const array = [];
-  // Initial data is a array of objects of all data, prizepool data is extracted and then put into another hash where the key is the prizepool and the value is the count of prizepools that it has seen. Then this data needs to be transformed into {x: key, y: value} format for victory.js Pie chart's data prop
+  let total = 0;
 
   data.forEach((obj) => {
+    total = total + 1;
     const prizePool = obj.prizePool;
-    console.log(`obj`, obj.prizePool);
     hash[prizePool] = hash[prizePool] ? hash[prizePool] + 1 : 1;
   });
 
-  for (const [key, value] of Object.entries(hash)) {
-    array.push({ x: key, y: value });
-  }
-
+  for (let [key, value] of Object.entries(hash)) {
+    array.push({
+      x: `${key} \n ${getPercentage(value, total)}%`,
+      y: value,
+    });
+  },
   return array;
 };
+
+const getPercentage = (numerator, total) =>
+  ((numerator / total) * 100).toFixed(2);
 
 export { transformForPrizepool };
